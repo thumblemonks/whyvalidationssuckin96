402b0793c8f23b77456e8ac6be0d75940a96a91e
ggironda <gabriel.gironda@centro.net>
2010-03-08 16:53:54 -0600

    adds an exception class

--- lib/whyvalidationssuckin96/constants.rb
+++ lib/whyvalidationssuckin96/constants.rb
@@ -1,2 +1,10 @@
 module WhyValidationsSuckIn96
-end
+  class ValidationError < RuntimeError
+    attr_reader :invalid_object
+
+    def initialize(message, invalid_object)
+      super(message)
+      @invalid_object = invalid_object
+    end
+  end # ValidationError
+end   # WhyValidationsSuckIn96
