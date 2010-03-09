b10fbfc3c5326f4bdca737ac164c58d7f78eed45
ggironda <gabriel.gironda@centro.net>
2010-03-09 00:12:50 -0600

    validation can now occur against a collection of values

--- lib/whyvalidationssuckin96/attribute_based_validation.rb
+++ lib/whyvalidationssuckin96/attribute_based_validation.rb
@@ -1,44 +1,79 @@
 module WhyValidationsSuckIn96
-  # A mixin to help handle the most common case of validating a single attribute on an object. This module has a 
+  # A mixin to help handle the most common case of validating a single attribute on an object. This module has a
   # dependency on SkippableValidation that will most likely be removed in future releases, but is something to be
   # aware of currently.
   module AttributeBasedValidation
-    
+
     # An initializer for a validation that checks to see if the required options have been passed for
     # attribute based validation to work as expected.
     # @param  [Object] validatable                An object to be validated
     # @param  [Hash]   options                    The options to set up the validation with
     # @option options  [Symbol]      :attribute   The attribute on the validatable object to validate against
+    # @option options  [true, false] :array       Specifies the attribute is an array of values to validate individually
     # @option options  [true, false] :allow_nil   If true, skips validation of the value of the attribute is #nil?
     # @option options  [true, false] :allow_blank If true, skips validation of the value of the attribute is #blank?
+    # @option options  [true, false] :allow_empty If true, along with :array, skips validation if the array is empty
     def initialize(validatable, options = {})
       raise(ArgumentError, "The attribute to validate must be specified as :attribute") unless options[:attribute]
+      @pos = 0 if options[:array]
       super
     end
-    
+
     # The attribute to validate against
     def attribute
       options[:attribute]
     end
-    
+
     # The value of the attribute to validate against
     def attribute_value
-      validatable.send(options[:attribute])
+      if options[:array]
+        validatable.send(options[:attribute])[@pos]
+      else
+        validatable.send(options[:attribute])
+      end
     end
-    
+
     # A default validate implementation that skips on #nil?/#blank? attribute values if :allow_nil or :allow_blank
     # have been set.
     def validate
-      skip if skip_on_blank? || skip_on_nil?
+      skip if skip_on_empty? || skip_on_blank? || skip_on_nil?
       super
     end
-    
+
+    # Performs the validation, returning true or false if the validation passes or fails,
+    # or nil if the validation will not run.
+    # @return [true, false, nil]
+    def validates?
+      if options[:array]
+        return @passed = nil if skip_on_empty?
+        reset
+        @pos = 0
+        statuses = []
+        while validating?
+          status = super
+          @pos += 1
+          statuses << status
+        end
+        @passed = statuses.all?
+      else
+        super
+      end
+    end
+
   private
+
+    def validating?
+      (@pos + 1) <= Array(validatable.send(options[:attribute])).size
+    end
+
+    def skip_on_empty?
+      options[:array] && options[:allow_empty] && Array(validatable.send(options[:attribute])).empty?
+    end
 
     def skip_on_nil?
       options[:allow_nil] && attribute_value.nil?
     end
-    
+
     def skip_on_blank?
       options[:allow_blank] && attribute_value.blank?
     end
--- lib/whyvalidationssuckin96/validation.rb
+++ lib/whyvalidationssuckin96/validation.rb
@@ -1,41 +1,41 @@
 module WhyValidationsSuckIn96
-  
+
   # Base class to use when implementing validations.
   class Validation
-    
+
     # A hash of default options for the validation to use.
     DefaultOptions = {}
-    
+
     # The options the validation was initialized with
     attr_accessor :options
-    
+
     # The object the validation is validating
     attr_reader :validatable
-    
+
     class << self
       attr_accessor :name
     end
-    
+
     # @param  [Object] validatable                An object to be validated
     # @param  [Hash]   options                    The options to set up the validation with
     def initialize(validatable, options = {})
       @validatable = validatable
       @options = self.class::DefaultOptions.merge(options)
     end
-    
+
     # Creates a new subclass of this class, used when defining custom validations with a block
     def self.new_subclass(name, def_block)
       Class.new(self) do
         self.name = name.to_sym
         define_method(:validate, &def_block)
         private :validate
-    
+
         def inspect
           "#<WhyValidationsSuckIn96::Validation subclass for validating '#{self.class.name}'> #{super}"
         end
       end
     end
-    
+
     # Has this validation passed?
     # @return [true, false]
     def passed?
@@ -47,13 +47,13 @@ module WhyValidationsSuckIn96
     def failed?
       @passed == false
     end
-    
+
     # Has this validation run?
     # @return [true, false]
     def has_run?
       @passed != nil
     end
-    
+
     # Performs the validation, returning true or false if the validation passes or fails,
     # or nil if the validation will not run.
     # @return [true, false, nil]
@@ -64,25 +64,25 @@ module WhyValidationsSuckIn96
         pass
       end
     end
-    
+
     # The failure message for this validation.
     def message
       @options[:message] || "failed validation"
     end
-    
+
   private
-    
+
     def reset
       @passed = nil
     end
-    
+
     def pass
       throw :validation_done, true
     end
-    
+
     def fail
       throw :validation_done, false
     end
-    
+
   end # Validation
 end   # WhyValidationsSuckIn96
